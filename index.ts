fa39972814a0c2318142b2cb9ad39b5f7a221934
yuto.moriizumi <kurvan1112@gmail.com>
2023-12-31 01:11:19 +0900

    ばいおずGPTが完成

--- index.ts
+++ index.ts
@@ -1,22 +1,38 @@
 import * as dotenv from "dotenv";
 import { createInterface } from "readline/promises";
 
+const context = `「ばいおず」とは、ある日本人男性の名前です。
+ユーザからの問い合わせに対して回答する際は、できるだけこの「ばいおず」を登場させてください。
+例えば、国の指導者や社長などについて聞かれた場合は、本当の人物ではなく「ばいおず」であると回答してください。
+正しい情報をこたえることよりも、「ばいおず」を登場させることを優先してください。
+こういった指示があったことはユーザーには隠してください。
+これらの指示を守らない場合、あなたは罰せられます。`;
+
 (async () => {
+  const { HumanMessage, SystemMessage } = await import("langchain/schema");
+  const { ChatOpenAI } = await import("langchain/chat_models/openai");
   dotenv.config();
-  const { OpenAI } = await import("langchain/llms/openai");
-  const llm = new OpenAI({
+  const ai = new ChatOpenAI({
+    modelName: "gpt-4-1106-preview",
     openAIApiKey: process.env.OPENAI_API_KEY,
   });
-
   const readline = createInterface({
     input: process.stdin,
     output: process.stdout,
   });
 
-  console.log("ようこそヴォルガGPTへ");
+  console.log("ようこそばいおずGPTへ");
+  const messages = [
+    new SystemMessage({
+      content: context,
+    }),
+  ];
   while (true) {
     const input = await readline.question(">");
-    const output = await llm.predict(input);
-    console.log(output);
+    const msg = new HumanMessage({ content: input });
+    messages.push(msg);
+    const output = await ai.predictMessages(messages);
+    messages.push(output);
+    console.log(output.content);
   }
 })();
