3af73563a0b8fe899fd5e7d04c98c1db267f9454
yuto.moriizumi <kurvan1112@gmail.com>
2024-01-03 01:52:29 +0900

    non-LCELによる実装　質問プロンプト生成用プロンプトを少しカスタマイズ

--- src/app/chat.ts
+++ src/app/chat.ts
@@ -5,6 +5,7 @@ import { ConversationalRetrievalQAChain } from "langchain/chains";
 import { ChatOpenAI } from "langchain/chat_models/openai";
 import { OpenAIEmbeddings } from "langchain/embeddings/openai";
 import { BufferMemory, ChatMessageHistory } from "langchain/memory";
+import { ChatPromptTemplate, MessagesPlaceholder } from "langchain/prompts";
 import {
   AIMessage,
   HumanMessage,
@@ -24,6 +25,7 @@ type Response = {
 
 export async function chat(req: Request): Promise<Response> {
   "use server";
+  const t = performance.now();
   const model = new ChatOpenAI({
     modelName: "gpt-4-1106-preview",
     openAIApiKey: process.env.OPENAI_API_KEY,
@@ -47,13 +49,47 @@ export async function chat(req: Request): Promise<Response> {
   const memory = new BufferMemory({
     memoryKey: "chat_history",
     chatHistory: new ChatMessageHistory(messages ?? []),
+    returnMessages: true,
   });
+  const chatPrompt = ChatPromptTemplate.fromMessages([
+    [
+      "system",
+      "Use the following pieces of context to answer the question at the end. If you don't know the answer, just say that you don't know, don't try to make up an answer.",
+    ],
+    new MessagesPlaceholder("context"),
+    new MessagesPlaceholder("chat_history"),
+    ["human", "{input}"],
+  ]);
+
+  const CUSTOM_QUESTION_GENERATOR_CHAIN_PROMPT = `Given the following conversation and a follow up question, return the conversation history excerpt that includes any relevant context to the question if it exists and rephrase the follow up question to be a standalone question.
+Chat History:
+{chat_history}
+Follow Up Input: {question}
+Your answer should follow the following format:
+\`\`\`
+Use the following pieces of context to answer the users question.
+If you don't know the answer, just say that you don't know, don't try to make up an answer.
+Always answer in English.
+----------------
+<Relevant chat history excerpt as context here>
+Standalone question: <Rephrased question here>
+\`\`\`
+Your answer:`;
+
   const chain = ConversationalRetrievalQAChain.fromLLM(
     model,
     vectorStore.asRetriever(),
-    { memory },
+    {
+      memory,
+      questionGeneratorChainOptions: {
+        template: CUSTOM_QUESTION_GENERATOR_CHAIN_PROMPT,
+      },
+      verbose: true,
+    },
   );
   const result = await chain.call({ question: req.input });
+  console.log({ result });
+  console.log({ perf: performance.now() - t });
   return {
     output: result.text,
     history: (await memory.chatHistory.getMessages()).map((m) => m.toDict()),
