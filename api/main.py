6cdd2a04969cdee8c4010c88aed96edf705cf0b6
yuto.moriizumi <kurvan1112@gmail.com>
2024-01-04 21:08:51 +0900

    python、fastapi、lambdaを追加

---
+++ api/main.py
@@ -0,0 +1,21 @@
+import uvicorn
+from fastapi import FastAPI
+from mangum import Mangum
+
+app = FastAPI()
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: str = None):
+    return {"item_id": item_id, "q": q}
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=4000)
+
+handler = Mangum(app)
